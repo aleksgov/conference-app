77700df1234886c5ba2acf44e1c7afa77d8de7f3
Aleks <aleksandrgovo@gmail.com>
2025-05-20 08:04:58 +0300

    fix(app): correct electron config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => {
-    return {
-        plugins: [react()],
-        server: {
-            proxy: {
-                '/status': 'http://localhost:8080',
-            }
-        },
-        define: {
-            'process.env.VITE_DEV_SERVER_URL': command === 'serve' ? '"http://localhost:5173"' : '""',
+export default defineConfig(({ command }) => ({
+    base: './',
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/status': 'http://localhost:8080'
         }
+    },
+    define: {
+        'process.env.VITE_DEV_SERVER_URL': command === 'serve' ? '"http://localhost:5173"' : '""'
+    },
+    build: {
+        outDir: 'dist'
     }
-})
+}));
